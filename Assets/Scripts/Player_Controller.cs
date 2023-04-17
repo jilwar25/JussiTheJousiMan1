2a03c0ed8c973549e6b154cefd2e79527aa986b3
Joni Lillsund <joni.lillsund@gmail.com>
2023-04-17 19:42:03 +0300

    Movement added

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,47 @@ using UnityEngine.InputSystem;
 
 public class Player_Controller : MonoBehaviour
 {
+    public Animator animator;
     public float walkSpeed = 5f;
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    private bool _isJumping = false;
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { 
+        get
+        {
+            return _isMoving;
+        } private set
+        {
+            _isMoving = value;
+            animator.SetBool("IsRunning", value);
+        } 
+    }
 
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+                _isFacingRight = value;
+        }
+    }
     Rigidbody2D rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -26,6 +58,7 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+     
         
     }
     private void FixedUpdate()
@@ -38,5 +71,24 @@ public class Player_Controller : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        setFacingDirection(moveInput);
+    }
+
+    private void setFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if(moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+
     }
 }
