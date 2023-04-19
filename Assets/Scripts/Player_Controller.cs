6855df7baecb125a76b341c3e8996c623d8a7544
Joni Lillsund <joni.lillsund@gmail.com>
2023-04-20 01:37:06 +0300

    Paskaa
    vittu

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,24 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirection))]
 public class Player_Controller : MonoBehaviour
 {
     public Animator animator;
     public float walkSpeed = 5f;
+    private float jumpImpulse = 5f;
     Vector2 moveInput;
+    TouchingDirection touchingDirection;
 
     private bool _isJumping = false;
     [SerializeField]
     private bool _isMoving = false;
-    public bool IsMoving { 
+    public bool IsMoving
+    {
         get
         {
             return _isMoving;
-        } private set
+        }
+        private set
         {
             _isMoving = value;
-            animator.SetBool("IsRunning", value);
-        } 
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
     }
 
     public bool _isFacingRight = true;
@@ -38,13 +43,14 @@ public class Player_Controller : MonoBehaviour
             {
                 transform.localScale *= new Vector2(-1, 1);
             }
-                _isFacingRight = value;
+            _isFacingRight = value;
         }
     }
     Rigidbody2D rb;
 
     private void Awake()
     {
+        touchingDirection = GetComponent<TouchingDirection>();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
@@ -52,18 +58,19 @@ public class Player_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-        
+
+
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * walkSpeed * Time.deltaTime, rb.velocity.y);
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -77,11 +84,11 @@ public class Player_Controller : MonoBehaviour
 
     private void setFacingDirection(Vector2 moveInput)
     {
-        if(moveInput.x > 0 && !IsFacingRight)
+        if (moveInput.x > 0 && !IsFacingRight)
         {
             IsFacingRight = true;
         }
-        else if(moveInput.x < 0 && IsFacingRight)
+        else if (moveInput.x < 0 && IsFacingRight)
         {
             IsFacingRight = false;
         }
@@ -89,6 +96,11 @@ public class Player_Controller : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-
+        // Tarkista onko elossa!!
+        if (context.started && touchingDirection.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jump);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
     }
 }
